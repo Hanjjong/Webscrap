c1b3edd86f69df028fdd83f5f28b12fb8af447fb
Replit user <>
2022-09-10 19:01:14 +0000

    Webscrap

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ get(f"{base_url}{search_term}")
 response = get(f"{base_url}{search_term}")
 
 if response.status_code != 200:
-  print(" Cant request website")
+  print(" Can't request website")
 else:  
   print(response.text)
